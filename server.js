d703eb3736e563639bac769a32beea341f221ff9
Payal Rani <bharadwajpayal143@gmail.com>
2026-04-21 20:52:41 +0530

    initial backend setup

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const os = require('os');
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hii! This is me!" });
+app.get('/api', (req, res) => {
+  res.json({
+    message: "Hii! This is me!",
+    server: os.hostname()
+  });
 });
-
 app.listen(3001, "0.0.0.0", () => {
   console.log("Server running on port 3001");
 });
